bb7c384ce96a3d272d1fae226cf38e7472706a0e
FelipeAugusto1303 <felipe.augusto.1303@gmail.com>
2023-09-21 01:35:03 -0400

    feat: create product card

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,9 +1,37 @@
+import { Flex, Image, Text } from "@chakra-ui/react";
 import React from "react";
 
 // import { Container } from './styles';
 
 const ProductCard: React.FC<any> = ({ product }) => {
-  return <div>{product.nome}</div>;
+  return (
+    <Flex
+      flexDirection="column"
+      alignItems="center"
+      gap="20px"
+      borderRadius="20px"
+      bg="#f5eeee"
+      height="300px"
+      width="200px"
+      marginX="10px"
+    >
+      <Image
+        src="./product_image.jpeg"
+        alt="product_image"
+        w="200px"
+        h="200px"
+        borderRadius="20px 20px 0px 0px"
+      />
+      <Flex flexDirection="column" w="100%" gap="5px" paddingLeft="10px">
+        <Text fontWeight="bold" fontSize="25px">
+          R$ {product.valor}
+        </Text>
+        <Text fontWeight="bold" fontSize="15px">
+          {product.nome}
+        </Text>
+      </Flex>
+    </Flex>
+  );
 };
 
 export default ProductCard;
--- src/pages/Product/Product.tsx
+++ src/pages/Product/Product.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { getProducts } from "../../services/productService";
+import { createProduct, getProducts } from "../../services/productService";
 import {
   Button,
   Divider,
@@ -8,57 +8,85 @@ import {
   Input,
   InputGroup,
   InputLeftElement,
+  useDisclosure,
 } from "@chakra-ui/react";
 import { AddIcon, SearchIcon } from "@chakra-ui/icons";
 import ProductCard from "../../components/ProductCard";
+import AddProductModal from "../../components/AddProductModal";
 
 const Product: React.FC = () => {
   const [products, setProducts] = useState([]);
   const [name, setName] = useState("");
+  const [updateList, setUpdateList] = useState(false);
+  const {
+    isOpen: isOpenAdd,
+    onClose: onCloseAdd,
+    onOpen: onOpenAdd,
+  } = useDisclosure();
 
   useEffect(() => {
     getProducts().then((response) => {
       console.log(response);
       setProducts(response.data.produtos);
     });
-  }, []);
-  return (
-    <Flex flexDirection="column" alignItems="center">
-      <Flex
-        bg="#85cc9c"
-        w="100%"
-        justifyContent="center"
-        alignItems="center"
-        p="20px"
-      >
-        <Heading as="h2" size="lg">
-          PUC-Rio MERCADO
-        </Heading>
-      </Flex>
-      <Flex p="20px" w="100%" gap="20px">
-        <InputGroup>
-          <InputLeftElement pointerEvents="none">
-            <SearchIcon color="gray.300" fontSize="20px" />
-          </InputLeftElement>
-          <Input
-            size="md"
-            placeholder="Busque um produto pelo seu nome"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-        </InputGroup>
-        <Button bg="#2b9eb3" color="#fff" leftIcon={<AddIcon />}>
-          Novo Produto
-        </Button>
-      </Flex>
-      <Divider w="95%" marginY="20px" />
+  }, [updateList]);
 
-      <Flex flexWrap="wrap">
-        {products.map((product) => {
-          return <ProductCard product={product} />;
-        })}
+  const handleCreateProduct = (body: any) => {
+    createProduct(body).then((response) => {
+      console.log(response);
+      setUpdateList((prev) => !prev);
+    });
+  };
+
+  return (
+    <>
+      <Flex flexDirection="column" alignItems="center">
+        <Flex
+          bg="#85cc9c"
+          w="100%"
+          justifyContent="center"
+          alignItems="center"
+          p="20px"
+        >
+          <Heading as="h2" size="lg">
+            PUC-Rio MERCADO
+          </Heading>
+        </Flex>
+        <Flex p="20px" w="100%" gap="20px">
+          <InputGroup>
+            <InputLeftElement pointerEvents="none">
+              <SearchIcon color="gray.300" fontSize="20px" />
+            </InputLeftElement>
+            <Input
+              size="md"
+              placeholder="Busque um produto pelo seu nome"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </InputGroup>
+          <Button
+            bg="#2b9eb3"
+            color="#fff"
+            leftIcon={<AddIcon />}
+            onClick={onOpenAdd}
+          >
+            Novo Produto
+          </Button>
+        </Flex>
+        <Divider w="95%" marginY="20px" />
+
+        <Flex flexWrap="wrap" w="100%" p="20px">
+          {products.map((product) => {
+            return <ProductCard product={product} />;
+          })}
+        </Flex>
       </Flex>
-    </Flex>
+      <AddProductModal
+        isOpen={isOpenAdd}
+        onClose={onCloseAdd}
+        handleCreate={handleCreateProduct}
+      />
+    </>
   );
 };
 
